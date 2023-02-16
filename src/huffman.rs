d4b3d0c9641758c5abab515c2dfecff2a0ece7c2
lakret <Rakuretu@gmail.com>
2023-02-16 21:24:55 +0100

    encoder & decoder hashmaps

--- src/huffman.rs
+++ src/huffman.rs
@@ -1,7 +1,9 @@
+use bit_vec::BitVec;
 use rayon::prelude::*;
 use std::{
     cmp::Reverse,
     collections::{BinaryHeap, HashMap},
+    hash::Hash,
 };
 
 pub fn learn_frequencies(lines: &Vec<String>) -> HashMap<char, i64> {
@@ -69,6 +71,43 @@ impl<T: Clone> Tree<T> {
             Node { right, .. } => Some(right),
             Leaf { .. } => None,
         }
+    }
+}
+
+impl<T: Eq + Clone + Hash> Tree<T> {
+    pub fn to_encoder(&self) -> HashMap<T, BitVec> {
+        let mut encoder = HashMap::new();
+
+        let mut stack = vec![(self, BitVec::new())];
+        while !stack.is_empty() {
+            let (node, path) = stack.pop().unwrap();
+            match node {
+                Leaf { token, .. } => {
+                    encoder.insert(token.clone(), path.clone());
+                }
+                Node { left, right, .. } => {
+                    let mut left_path = path.clone();
+                    left_path.push(false);
+                    stack.push((left, left_path));
+
+                    let mut right_path = path.clone();
+                    right_path.push(true);
+                    stack.push((right, right_path));
+                }
+            }
+        }
+
+        encoder
+    }
+
+    pub fn to_decoder(&self, encoder: Option<HashMap<T, BitVec>>) -> HashMap<BitVec, T> {
+        let encoder = encoder.unwrap_or(self.to_encoder());
+
+        let mut decoder = HashMap::new();
+        for (token, prefix) in encoder {
+            decoder.insert(prefix, token);
+        }
+        decoder
     }
 }
 
