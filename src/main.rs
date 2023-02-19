facc9d79fb7bbfc45d93fbd811b925331d406937
lakret <Rakuretu@gmail.com>
2023-02-19 18:20:36 +0100

    renamed crate to huffman-rs & added CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,104 @@
+use clap::{Parser, ValueEnum};
 use std::fs::{self, File};
 use std::io::Write;
+use std::path::PathBuf;
 use std::time;
 
 mod compression;
 mod freqs;
 mod huffman;
 
-const DATA_PATH: &'static str = "data/wikisent2.txt";
-const WORDS_OUT_PATH: &'static str = "data/words.huffman";
-const CHARS_OUT_PATH: &'static str = "data/chars.huffman";
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(value_enum)]
+    action: Action,
+    #[arg(value_enum)]
+    mode: Mode,
+    input: PathBuf,
+    output: PathBuf,
+}
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+enum Action {
+    Compress,
+    Extract,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+enum Mode {
+    Words,
+    Chars,
+}
+
+// cargo run --release -- compress words data/wikisent2.txt data/words.huffman
+// cargo run --release -- extract words data/words.huffman data/extracted.txt
+// (to check if the extraction was correct: `diff data/wikisent2.txt data/extracted.txt`)
+//
+// cargo run --release -- compress chars data/wikisent2.txt data/chars.huffman
+// cargo run --release -- extract chars data/chars.huffman data/extracted.txt
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let timer = time::Instant::now();
-    let text = fs::read_to_string(DATA_PATH)?;
-    let lines: Vec<_> = text.split('\n').map(|x| x.to_string()).collect();
-    let time = timer.elapsed();
-    let lines_count = lines.len();
-    println!("Read the source file with {lines_count} lines in {time:?}");
+    let args = Args::parse();
+    dbg!(&args);
 
-    // Words compress & decompress
-    let timer = time::Instant::now();
-    let compressed = compression::compress(&lines, freqs::word_frequencies, |line| {
-        line.split_ascii_whitespace().map(|token| token.to_string())
-    })?;
-    let time = timer.elapsed();
-    println!("Compressed as words in {time:?}");
+    match args.action {
+        Action::Compress => {
+            let timer = time::Instant::now();
+            let text = fs::read_to_string(args.input)?;
+            let lines: Vec<_> = text.split('\n').map(|x| x.to_string()).collect();
+            let time = timer.elapsed();
+            let lines_count = lines.len();
+            println!("Read the source file with {lines_count} lines in {time:?}");
 
-    let timer = time::Instant::now();
-    let mut out_f = File::create(WORDS_OUT_PATH)?;
-    out_f.write(&compressed)?;
-    let time = timer.elapsed();
-    println!("Wrote to {WORDS_OUT_PATH} in {time:?}");
+            let timer = time::Instant::now();
+            let compressed = match args.mode {
+                Mode::Words => compression::compress(&lines, freqs::word_frequencies, |line| {
+                    line.split_ascii_whitespace().map(|token| token.to_string())
+                }),
+                Mode::Chars => {
+                    compression::compress(&lines, freqs::char_frequencies, |line| line.chars())
+                }
+            }?;
+            let time = timer.elapsed();
+            println!("Compressed as {mode:?} in {time:?}.", mode = args.mode);
 
-    let timer = time::Instant::now();
-    let data = fs::read(WORDS_OUT_PATH)?;
-    let time = timer.elapsed();
-    println!("Read the compressed file in {time:?}");
+            let timer = time::Instant::now();
+            let mut out_f = File::create(&args.output)?;
+            out_f.write(&compressed)?;
+            let time = timer.elapsed();
+            println!(
+                "Wrote to {output_path:?} in {time:?}",
+                output_path = args.output
+            );
+        }
+        Action::Extract => {
+            let timer = time::Instant::now();
+            let data = fs::read(&args.input)?;
+            let time = timer.elapsed();
+            println!("Read the compressed file in {time:?}");
 
-    let timer = time::Instant::now();
-    let res_lines = compression::decompress(&data, |tokens: Vec<String>| tokens.join(" "))?;
-    let time = timer.elapsed();
-    let res_lines_count = res_lines.len();
-    println!("Decompressed file with {res_lines_count} lines in {time:?}");
+            let timer = time::Instant::now();
+            let content = match args.mode {
+                Mode::Words => {
+                    compression::decompress(&data, |tokens: Vec<String>| tokens.join(" "))?
+                }
+                Mode::Chars => compression::decompress(&data, |tokens: Vec<char>| {
+                    tokens.into_iter().collect()
+                })?,
+            };
+            let time = timer.elapsed();
+            let lines_count = content.len();
+            println!("Extracted file with {lines_count} lines in {time:?}.");
 
-    dbg!(&res_lines[..4]);
-    dbg!(&res_lines[(res_lines.len() - 4)..]);
-
-    println!();
-
-    // Chars compress & decompress
-    let timer = time::Instant::now();
-    let compressed = compression::compress(&lines, freqs::char_frequencies, |line| line.chars())?;
-    let time = timer.elapsed();
-    println!("Compressed as chars in {time:?}");
-
-    let timer = time::Instant::now();
-    let mut out_f = File::create(CHARS_OUT_PATH)?;
-    out_f.write(&compressed)?;
-    let time = timer.elapsed();
-    println!("Wrote to {CHARS_OUT_PATH} in {time:?}");
-
-    let timer = time::Instant::now();
-    let data = fs::read(CHARS_OUT_PATH)?;
-    let time = timer.elapsed();
-    println!("Read the compressed file in {time:?}");
-
-    let timer = time::Instant::now();
-    let res_lines =
-        compression::decompress(&data, |tokens: Vec<char>| tokens.into_iter().collect())?;
-    let time = timer.elapsed();
-    let res_lines_count = res_lines.len();
-    println!("Decompressed file with {res_lines_count} lines in {time:?}");
-
-    dbg!(&res_lines[..4]);
-    dbg!(&res_lines[(res_lines.len() - 4)..]);
+            let timer = time::Instant::now();
+            fs::write(&args.output, content.join("\n"))?;
+            let time = timer.elapsed();
+            println!(
+                "Wrote to {output_path:?} in {time:?}",
+                output_path = args.output
+            );
+        }
+    }
 
     Ok(())
 }
